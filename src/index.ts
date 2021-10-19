bbbf769a0a852dcf86216678216fa63124573331
Oleg Konan <o.konan@softteco.com>
2021-10-19 18:48:19 +0300

    fix port

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import { router as dataRouter } from './routes/dataRouter';
+const PORT = process.env.PORT || 6001;
 
 const app = express();
 app.use('/', dataRouter)
 
-app.listen(5500, () => console.log('Server is running...'));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
