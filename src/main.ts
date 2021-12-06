b78d0bd47cfd1b0feb79f8397906fdc55935355b
Oleg Konan <o.konan@softteco.com>
2021-12-06 16:27:47 +0300

    fix port

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module';
 
+const PORT = process.env.PORT || 6000
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule)
+  await app.listen(PORT)
 }
 
 bootstrap();
